83f61f83f858cf27cc33260a12f29e351c396e97
Sneha Dilip <smidil.eim.ae@tamu.edu>
2023-08-24 06:53:57 -0500

    Works with user input

---
+++ index.js
@@ -0,0 +1,77 @@
+
+
+//music plays when button is clicked
+var num_of_buttn = document.querySelectorAll(".drum").length;
+for(var i =0;i<num_of_buttn;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var html_btn = this.innerHTML;
+        make_sound(html_btn);
+        btn_animation(html_btn);
+    });
+    
+    
+}
+
+//music plays when key is pressed
+document.addEventListener("keypress",function(event){
+    make_sound(event.key);
+    btn_animation(event.key);
+});
+
+
+function make_sound(key){
+         console.log(key);
+        
+        switch(key){
+            case "w":
+                var crash = new Audio('./sounds/crash.mp3');
+                crash.play();
+                break;
+    
+            case "a":
+                var kick = new Audio('./sounds/kick-bass.mp3');
+                kick.play();
+                break;
+    
+            case "s":
+                var snare = new Audio('./sounds/snare.mp3');
+               snare.play();
+                break;
+    
+            case "d":
+                var tom1 = new Audio('./sounds/tom-1.mp3');
+                tom1.play();
+                break;
+    
+            case "j":
+                var tom2 = new Audio('./sounds/tom-2.mp3');
+                tom2.play();
+                break;
+    
+            case "k":
+                var tom3 = new Audio('./sounds/tom-3.mp3');
+                tom3.play();
+                break;
+    
+            case "l":
+                var tom4 = new Audio('./sounds/tom-4.mp3');
+                tom4.play();
+                break;
+    
+            default: alert("wrong button");
+        }
+
+}
+
+// button animation
+
+function btn_animation(key){
+    // finding the class that is associated with that letter or key eg. w,a,s..
+    var class_names = document.querySelector("."+key);
+    // adding the new class that will change the style of the button when it is clicked
+    class_names.classList.add("pressed");
+    // to remove the style of that button after it is class (0.1s)
+    setTimeout(function(){
+        class_names.classList.remove("pressed");
+    },100);
+}
